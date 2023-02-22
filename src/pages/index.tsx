27ef919745f96923a846b4c68deb47dbac88ae6b
Film-Legion <121986283+HFXFL@users.noreply.github.com>
2023-02-22 19:02:10 +0000

    Graphic Design 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,45 +49,44 @@ export default function Home (){
       />
       <MdAccountCircle size={'30px'} color='#55ff00' />
       <IoSearchSharp size={'30px'} color='#55ff00' />
-      <IoMenuSharp size={'30px'} color='#55ff00' />
+      <IoMenuSharp size={'30px'} color='#ffffff' />
     </HStack>
 <Image
-    width={'100vw'}
-    height={'50vw'}
+    width={'120%'}
+    height={'120%'}
     mb={'40px'}
-    src='https://res.cloudinary.com/ddyecvz0i/image/upload/v1676410495/FL/image001_1_gmdscx.png'
+    src='https://res.cloudinary.com/ddyecvz0i/image/upload/v1677090721/FL/BG_Mobile_pfiskh.png'
     alt='ScannanImage'
   /> 
         <div className={styles.title}
           >
-            <h3>
+            <h2>
             Here is your chance to win a grand prize, from $1,000! 
-            </h3>
+            </h2>
         </div>
         
 
         <div className={styles.title}>
-            <h3>
+            <h2>
             Crowdfund this film and raise awareness of mental health.
-            </h3>
+            </h2>
             <div className={styles.description}>
-            <p>
+            <h2>
             We are using NFTs differently, are you up to the challenge? This is your chance to win a cash or crypto prize with the Scannan series and Film-Legion. Each NFT cost between ¢50 and $1.20 (in MATIC), and you will be assisting in the funding of critical research into severe, enduring mental health and physical health disorders. You will also be crowdfunding the production of Scannan, a 3-part feature length series, 30 YEARS in the making. Only on FilmLegion.com
-
-            </p>
+            </h2>
         </div>
         </div>
  
         <div className={styles.description}>
-        <p>
+        <h2>
             A single cryptic clue is hidden within these images. You must collect all of the correct characters to recieve a secret question. Follow the questions to get closer to the ever growing prize, and a guarenteed $1,000. Please sell at least 2 NFTs to WIN, and sell more so others may participate and raise money for producing Scannan, and fund these worthy causes.
-        </p>
+        </h2>
         </div>
 
         <div className={styles.nftTitle}>
-        <p>
+        <h2>
           See The Collections
-        </p>
+        </h2>
         </div>
 
 
