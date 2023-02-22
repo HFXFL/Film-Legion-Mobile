02b10f9c059f47b68f2bdf1563368238cfab38f6
Film-Legion <121986283+HFXFL@users.noreply.github.com>
2023-02-22 19:40:26 +0000

    Graphic Design 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import styles from '@/styles/Custom.module.css';
-import { Button, HStack, Image } from "@chakra-ui/react";
+import { Box, Button, HStack, Image } from "@chakra-ui/react";
 import Head from "next/head";
 import {
   IoLogoInstagram,
@@ -34,7 +34,7 @@ export default function Home (){
       <IoLogoYoutube size={'20px'} color='#bab3b3' />
       <IoLogoTwitter size={'20px'} color='#bab3b3' />
       <IoLogoInstagram size={'20px'} color='#bab3b3' />
-      <Button colorScheme={'green'}>METAMASK</Button>
+      <Button bg={'#55ff00'} border={'1px'} colorScheme={'green'}>METAMASK</Button>
     </HStack>
       <hr/>
 
@@ -67,28 +67,42 @@ export default function Home (){
         
 
         <div className={styles.title}>
-            <h2>
+            <h3>
             Crowdfund this film and raise awareness of mental health.
-            </h2>
+            </h3>
             <div className={styles.description}>
-            <h2>
+            <h4>
             We are using NFTs differently, are you up to the challenge? This is your chance to win a cash or crypto prize with the Scannan series and Film-Legion. Each NFT cost between ¢50 and $1.20 (in MATIC), and you will be assisting in the funding of critical research into severe, enduring mental health and physical health disorders. You will also be crowdfunding the production of Scannan, a 3-part feature length series, 30 YEARS in the making. Only on FilmLegion.com
-            </h2>
+            </h4>
         </div>
         </div>
  
         <div className={styles.description}>
-        <h2>
+        <h4>
             A single cryptic clue is hidden within these images. You must collect all of the correct characters to recieve a secret question. Follow the questions to get closer to the ever growing prize, and a guarenteed $1,000. Please sell at least 2 NFTs to WIN, and sell more so others may participate and raise money for producing Scannan, and fund these worthy causes.
-        </h2>
+        </h4>
         </div>
 
         <div className={styles.nftTitle}>
         <h2>
           See The Collections
         </h2>
-        </div>
 
+        </div>
+        <Box w={'100vw'} h={'100vw'} bg={'#ffffff'}>
+        <Box w={'10vw'} h={'10vw'} bg='GrayText'></Box>
+
+
+
+
+
+
+
+
+
+
+
+        </Box>
 
     </>
   )
